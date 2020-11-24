7b8631e04987b09930cace2ec2eafc0e582962da
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-11-24 00:25:42 -0800

    Adds main code backbone

--- index.js
+++ index.js
@@ -1,15 +1,155 @@
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the project title? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('You need to enter a project title!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'What is the project description? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('You need to enter a project description!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('You need to enter installation instructions!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Please describe the usage information (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('You need to enter the usage information!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'list-input',
+    name: 'license',
+    message: 'Please select a license (Optional)',
+    choices: ['ISC', 'MIT', 'No License']
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'What are the contribution guidelines? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('You need to enter the contribution guidelines!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What are the test instructions? (Optional)',
+  },
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('You need to enter your name!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'What is your GitHub username? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('You need to enter your GitHub username!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('You need to enter your email address!');
+        return false;
+      }
+    }
+  }
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(pageMarkdown) {
+  return new Promise((resolve, reject) => {
+    fs.writeToFile('./output/README.md', data, error => {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'README.md created!'
+      });
+    });
+  });
+};
 
 // function to initialize program
 function init() {
-
+  inquirer
+    .prompt(questions)
+    .then(readmeData => {
+      return generateMarkdown(readmeData);
+    })
+    .then(pageMarkdown => {
+      return writeToFile(pageMarkdown);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,23 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  
+  ## ${data.description}
+  
+  ## ${data.installation}
+  
+  ## ${data.usage}
+  
+  ## ${data.license}
+  
+  ## ${data.contribution}
+  
+  ## ${data.test}
+  
+  ### ${data.name}
+  ### ${data.username}
+  ### ${data.email}
 
 `;
 }
