3e2e94a5c433c1850440ddf76aad068720764337
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-11-28 15:34:02 -0800

    Adds more badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -172,6 +172,10 @@ function generateMarkdown(data) {
   return `# ${ data.title }
 
 ${ generateLicenseBadge(data) }
+![GitHub repo size](https://img.shields.io/github/repo-size/USER/REPOSITORY?style=plastic)
+![GitHub language count](https://img.shields.io/github/languages/count/USER/REPOSITORY?style=plastic)
+![GitHub top language](https://img.shields.io/github/languages/top/USER/REPOSITORY?style=plastic)
+![GitHub last commit](https://img.shields.io/github/last-commit/USER/REPOSITORY?color=red&style=plastic)
 
 ## Description
 ${ data.description }
